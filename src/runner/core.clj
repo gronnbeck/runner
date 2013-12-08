859ae83da75764259a78f5a6bb2a1658abc778c7
Ken Grønnbeck <ken.gronnbeck@bekk.no>
2013-12-08 15:59:34 +0100

    [robustness] able to handle tasks that throws exceptions

--- src/runner/core.clj
+++ src/runner/core.clj
@@ -1,17 +1,21 @@
 (ns runner.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-(defstruct job_desc :id :name :tasks)
-(defstruct job :id :input :job_desc_id)
+(defstruct desc :id :name :tasks)
+(defstruct job :id :input :desc_id)
 (defstruct result :id :status :output)
 
 (defn execute [job]
-	(struct-map result 
-		:id (job :id)
-		:status "Success"
-		:output (map #(%) (job :tasks)))) 
+	(let [output (map #(%) (job :tasks))]
+		(try 
+			(struct-map result 
+				:id (job :id)
+				:status "Success" 
+				:output (doall output))
+		(catch Exception e  
+			(struct-map result 
+				:id (job :id)
+				:status "Error" 
+				:output nil)))))
+
+	
 
--- test/runner/core_test.clj
+++ test/runner/core_test.clj
@@ -2,16 +2,29 @@
   (:require [clojure.test :refer :all]
             [runner.core :refer :all]))
 
-(def desc_with_println (struct-map job_desc 
-			:id 1230213 								;; should be a unique id
-			:name "This is a job name" 	;; self explanatory
-			:tasks (list #(println 1)) 							;; list of tasks
-			))
+(def desc_with_println 
+	(struct-map desc 
+			:id 1230213 								 ;; should be a unique id
+			:name "This is a job name" 	 ;; self explanatory
+			:tasks (list #(println 1)))) ;; list of tasks
+
+(def desc_with_calcs
+	(struct-map desc 
+		:id 144444
+		:name "This is a calculation"
+		:tasks (list #(+ 1 2) #(+ 1 2) #(+ 1 2))))
+
+(def desc_with_exception 
+	(struct-map desc
+		:id 121212
+		:name "This job should fail"
+		:tasks (list #(throw (Exception. "This is an exception")))))
 
 ;; Learning how to access structures in clojure
 (deftest struct_test
 	(testing "Testing if id is set to correct value"
 		(is (= (desc_with_println :id) 1230213)))
+
 	(testing "Testing if name is set to correct value"
 		(is (= (desc_with_println :name) "This is a job name"))))
 
@@ -19,9 +32,15 @@
 	(testing "Execute task without input successfully" 
 		(let [result (execute desc_with_println)]
 			(is (= (result :output) (list nil)))
-			(is (= (result :status) "Success")))))
+			(is (= (result :status) "Success"))))
+	
+	(testing "Testing that the correct value is outputted"
+		(is (= ((execute desc_with_calcs) :output) (list 3 3 3))))
 
-
+	(testing "Execute task that throws an exception"
+		(let [result (execute desc_with_exception)]
+			(is (= (result :output) nil))
+			(is (= (result :status) "Error")))))
 
 
 
